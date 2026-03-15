582c36186fc957133002b48525b3ecd2983fb23b
this-click <88038974+this-click@users.noreply.github.com>
2026-03-15 15:51:14 +0100

    Fix comment

--- Core/Dtos/FeedTopperDto.cs
+++ Core/Dtos/FeedTopperDto.cs
@@ -1,14 +1,13 @@
 ﻿namespace backend.Core.Dtos
 {
     /// <summary>
-    /// Data Transport Object for feed topper, which is a batch PATCH request.
+    /// Data Transport Object for feed topper.
     ///     
-    /// TODO: Fix comment
     ///     - FedDate is null if the topper was never fed. 
-    ///     - FedDate is not null if it was fed and it's in the system.
-    ///     - when user feeds the topper, FedDate is set and topper priority decreases.
+    ///     - FedDate is not null if it was fed before.
+    ///     - When user feeds the topper, FedDate is set and topper priority decreases.
     ///     
-    /// Both dates are required currently for scalability, for when ToppersController is required to set other dates besides today. ???
+    /// FedDate is required currently for APP v2, when ToppersController is required to set other dates besides today.
     /// </summary>
     public class FeedTopperDto
     {
